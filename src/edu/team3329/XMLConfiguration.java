418699eda4af4c1696a9f8b17e70d7d7afc51206
Ben Davenport-Ray <laserdude11@gmail.com>
2011-11-21 21:14:40 -0500

    Added a Configuration class for dem configuration files.

--- src/edu/team3329/XMLConfiguration.java
+++ src/edu/team3329/XMLConfiguration.java
@@ -1,13 +1,15 @@
 package edu.team3329;
 
-import 
+
+import com.sun.squawk.io.BufferedReader;
 
 class XMLConfiguration{
+	BufferedReader in;	
 
-	public XMLConfiguration{
-	
-	
+	public XMLConfiguration(){
+		in = new BufferedReader(new FileReader("config.xml"));
 	}
+
 	public String getString(String key){
 			
 	}
