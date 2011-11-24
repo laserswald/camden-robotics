7d71943a01075e95687d2f937c3c88c35e4b7955
Ben Davenport-Ray <laserdude11@gmail.com>
2011-11-23 22:27:30 -0500

    New branch, messed with configuration stuff.
    I'm just going to work on the config stuff in this branch.

--- src/edu/team3329/config/Configuration.java
+++ src/edu/team3329/config/Configuration.java
@@ -1,9 +1,9 @@
 package edu.team3329;
 
-
+import java.util.Hashtable;
 import com.sun.squawk.io.BufferedReader;
 
-class Configuration{
+public class Configuration{
 	BufferedReader in;	
 
 	public Configuration(){
@@ -13,4 +13,25 @@ class Configuration{
 	public String getString(String key){
 			
 	}
+
+	public double getDouble(String key){
+	
+	}
+
+	private parse(){
+		while((thisline== br.readLine())!=null){
+			
+		}
+	}
+}
+
+private class ConfigSection {
+	private String name;
+	private Hashtable data;
+	
+	public ConfigSection(){
+		this.name = "default"
+	}
+	public void 
+
 }
